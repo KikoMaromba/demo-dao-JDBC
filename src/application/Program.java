5d7df98451d5fbba29e58e7d6cae63780477f94d
KikoMaromba <Kaiquebragasantos@gmail.com>
2022-09-08 09:52:45 -0300

    Implementação do FindALL

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,16 +10,22 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerdao = DaoFactory.createSellerDao();
-		
-		System.out.println("=== Test 1: seller FindById ====");
+
+		System.out.println("\n=== Test 1: seller FindById ====");
 		Seller seller = sellerdao.findById(5);
-		
 		System.out.println(seller);
+
 		System.out.println("\n=== Test 2: Deparment FindByDepartment ====");
 		Department dep = new Department(2, null);
 		List<Seller> list = sellerdao.findByDepartment(dep);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n=== Test 3: Deparment FindALL ====");
+		list = sellerdao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
