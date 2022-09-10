d8941bd1a0a2b369e2710ef791d42525563ade45
KikoMaromba <Kaiquebragasantos@gmail.com>
2022-09-10 08:52:39 -0300

    Insert Seller implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -24,11 +26,15 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("\n=== Test 3: Deparment FindALL ====");
+		System.out.println("\n=== Test 3: Seller FindALL ====");
 		list = sellerdao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 4: Seller Insert ====");
+		Seller newSeller = new Seller(null, "Greg Wolf", "Greg@Gmail", new Date(), 4000.00, dep);
+		sellerdao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
-
 }
